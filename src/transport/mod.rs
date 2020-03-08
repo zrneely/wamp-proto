8739098c77ec5c848cb1c43b0c8fb656d07bd540
Zachary Neely <zrussellneely@gmail.com>
2020-03-07 20:41:59 -0800

    Make it easier to use TransportableValue

--- src/transport/mod.rs
+++ src/transport/mod.rs
@@ -80,9 +80,17 @@ pub enum TransportableValue {
 }
 impl TransportableValue {
     /// Attempts to get the value, assuming it's an integer.
-    pub fn into_int(self) -> Option<u64> {
+    pub fn as_int(&self) -> Option<u64> {
         match self {
-            TransportableValue::Integer(x) => Some(x),
+            TransportableValue::Integer(x) => Some(*x),
+            _ => None,
+        }
+    }
+
+    /// Attempts to get the value, assuming it's a String.
+    pub fn as_str(&self) -> Option<&str> {
+        match self {
+            TransportableValue::String(ref x) => Some(x.as_str()),
             _ => None,
         }
     }
@@ -96,9 +104,17 @@ impl TransportableValue {
     }
 
     /// Attempts to get the value, assuming it's a boolean.
-    pub fn into_bool(self) -> Option<bool> {
+    pub fn as_bool(&self) -> Option<bool> {
         match self {
-            TransportableValue::Bool(x) => Some(x),
+            TransportableValue::Bool(x) => Some(*x),
+            _ => None,
+        }
+    }
+
+    /// Attempts to get the value, assuming it's a list.
+    pub fn as_list(&self) -> Option<&Vec<TransportableValue>> {
+        match self {
+            TransportableValue::List(x) => Some(x),
             _ => None,
         }
     }
@@ -107,6 +123,14 @@ impl TransportableValue {
     pub fn into_list(self) -> Option<Vec<TransportableValue>> {
         match self {
             TransportableValue::List(x) => Some(x),
+            _ => None,
+        }
+    }
+
+    /// Attempts to get the value, assuming it's a dictionary.
+    pub fn as_dict(&self) -> Option<&HashMap<String, TransportableValue>> {
+        match self {
+            TransportableValue::Dict(x) => Some(x),
             _ => None,
         }
     }
@@ -128,44 +152,58 @@ mod tests {
     #[test]
     fn transportable_value_test() {
         let tv = TransportableValue::Bool(true);
-        assert_eq!(Some(true), tv.clone().into_bool());
+        assert_eq!(Some(true), tv.as_bool());
         assert_eq!(None, tv.clone().into_dict());
-        assert_eq!(None, tv.clone().into_int());
+        assert_eq!(None, tv.as_dict());
+        assert_eq!(None, tv.as_int());
         assert_eq!(None, tv.clone().into_list());
-        assert_eq!(None, tv.into_string());
+        assert_eq!(None, tv.as_list());
+        assert_eq!(None, tv.clone().into_string());
+        assert_eq!(None, tv.as_str());
 
         let tv = TransportableValue::Dict(Default::default());
-        assert_eq!(None, tv.clone().into_bool());
+        assert_eq!(None, tv.as_bool());
         assert_eq!(
             Some(std::collections::HashMap::new()),
             tv.clone().into_dict()
         );
-        assert_eq!(None, tv.clone().into_int());
+        assert!(tv.as_dict().unwrap().is_empty());
+        assert_eq!(None, tv.as_int());
         assert_eq!(None, tv.clone().into_list());
-        assert_eq!(None, tv.into_string());
+        assert_eq!(None, tv.as_list());
+        assert_eq!(None, tv.clone().into_string());
+        assert_eq!(None, tv.as_str());
 
         let tv = TransportableValue::Integer(12345);
-        assert_eq!(None, tv.clone().into_bool());
+        assert_eq!(None, tv.as_bool());
         assert_eq!(None, tv.clone().into_dict());
-        assert_eq!(Some(12345), tv.clone().into_int());
+        assert_eq!(None, tv.as_dict());
+        assert_eq!(Some(12345), tv.as_int());
         assert_eq!(None, tv.clone().into_list());
-        assert_eq!(None, tv.into_string());
+        assert_eq!(None, tv.as_list());
+        assert_eq!(None, tv.clone().into_string());
+        assert_eq!(None, tv.as_str());
 
         let tv = TransportableValue::List(vec![TransportableValue::Integer(12345)]);
-        assert_eq!(None, tv.clone().into_bool());
+        assert_eq!(None, tv.as_bool());
         assert_eq!(None, tv.clone().into_dict());
-        assert_eq!(None, tv.clone().into_int());
+        assert_eq!(None, tv.as_dict());
+        assert_eq!(None, tv.as_int());
         assert_eq!(
             Some(vec![TransportableValue::Integer(12345)]),
             tv.clone().into_list()
         );
+        assert_eq!(12345, tv.as_list().unwrap()[0].as_int().unwrap());
         assert_eq!(None, tv.into_string());
 
         let tv = TransportableValue::String("asdf".into());
-        assert_eq!(None, tv.clone().into_bool());
+        assert_eq!(None, tv.as_bool());
         assert_eq!(None, tv.clone().into_dict());
-        assert_eq!(None, tv.clone().into_int());
+        assert_eq!(None, tv.as_dict());
+        assert_eq!(None, tv.as_int());
         assert_eq!(None, tv.clone().into_list());
-        assert_eq!(Some("asdf".into()), tv.into_string());
+        assert_eq!(None, tv.as_list());
+        assert_eq!(Some("asdf".into()), tv.clone().into_string());
+        assert_eq!(Some("asdf"), tv.as_str());
     }
 }
--- tests/integration/subscribe/mod.rs
+++ tests/integration/subscribe/mod.rs
@@ -46,24 +46,18 @@ async fn subscribe_recv_unsubscribe() {
         .unwrap();
 
     let peer = start_peer("subscribe", "subscribeRecvUnsubscribe", &router).await;
-    println!("peer ready");
+    println!("peer sent message");
 
     let message = subscription.next().await.unwrap();
     assert_eq!(message.arguments.len(), 1);
     let message_arg_0 = message.arguments[0].clone().into_dict().unwrap();
-    assert_eq!(
-        message_arg_0.get("a").unwrap().clone().into_int().unwrap(),
-        1
-    );
-    assert_eq!(
-        message_arg_0.get("b").unwrap().clone().into_bool().unwrap(),
-        false
-    );
+    assert_eq!(message_arg_0.get("a").unwrap().as_int().unwrap(), 1);
+    assert_eq!(message_arg_0.get("b").unwrap().as_bool().unwrap(), false);
     let list = message_arg_0.get("c").unwrap().clone().into_list().unwrap();
     assert_eq!(list.len(), 3);
-    assert_eq!(list[0].clone().into_int().unwrap(), 3);
-    assert_eq!(list[1].clone().into_int().unwrap(), 4);
-    assert_eq!(list[2].clone().into_int().unwrap(), 5);
+    assert_eq!(list[0].as_int().unwrap(), 3);
+    assert_eq!(list[1].as_int().unwrap(), 4);
+    assert_eq!(list[2].as_int().unwrap(), 5);
 
     client.unsubscribe(subscription).await.unwrap();
 
